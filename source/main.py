e48025b57b8f1fc18ca0ef40c9ce826dbe703d7e
JakeTheRealOne <jakelevrai@outlook.be>
2024-07-02 22:43:30 +0200

    fixing small bugs with up and down movs

--- source/main.py
+++ source/main.py
@@ -38,8 +38,10 @@ help_msg = ("Help page:\n  Welcome to 2048! Programmed by JakeTheRealOne"
 + " a game in Azerty mode\n    --qwerty : Start a game in Qwerty mode"
 + "\n    --vim : Start a game as a GigaChad\n  Credit: the author of the game is Gabriele Cirulli")
 
+
 class GameError(Exception):
     """represent a 2048 game intended exception"""
+    pass
 
 class Game:
     """represent a game of 2048"""
@@ -154,27 +156,49 @@ class Game:
         """ TEMPORARY
         change the gravity to the down gravity
         """
+        # for column in range(self._width):
+        #     for case in range(self._height - 2, -1, -1):
+        #         current_pos = origin_pos = case * self._width + column
+        #         if current_pos in self._free_spots:
+        #             continue
+        #         current_pos += self._width
+        #         while current_pos in self._free_spots:
+        #             current_pos += self._width
+        #         current_pos -= self._width
+        #         new_case = current_pos // self._width
+        #         if origin_pos != current_pos:
+        #             self._grid[case][column], self._grid[new_case][column] = self._grid[new_case][column], self._grid[case][column]
+        #             # update free_spots
+        #             self._free_spots.remove(current_pos)
+        #             self._free_spots.add(origin_pos)
+        #         # merge with the bottom tile if the values are identical
+        #         if new_case < self._height - 1 and self._grid[new_case][column] == self._grid[new_case+1][column]:
+        #             # WHILE INSTEAD OF IF ? (in the real game its just an if but we can try our own rice here)
+        #             self._grid[new_case][column] = 0
+        #             self._grid[new_case + 1][column] += 1
+        #             self._free_spots.add(current_pos)
         for column in range(self._width):
             for case in range(self._height - 2, -1, -1):
-                current_pos = origin_pos = case * self._width + column
-                if current_pos in self._free_spots:
+                y_pos = case
+                # stop if case is empty
+                pos = column + case * self._width
+                if pos in self._free_spots:
                     continue
-                current_pos += self._width
-                while current_pos in self._free_spots:
-                    current_pos += self._width
-                current_pos -= self._width
-                new_case = current_pos // self._width
-                if origin_pos != current_pos:
-                    self._grid[case][column], self._grid[new_case][column] = self._grid[new_case][column], self._grid[case][column]
+                # the tile falls
+                while y_pos < self._height - 1 and pos + self._width in self._free_spots:
+                    y_pos += 1
+                    pos += self._width
+                if case != y_pos:
+                    # update position in the grid
+                    self._grid[case][column], self._grid[y_pos][column] = self._grid[y_pos][column], self._grid[case][column]
                     # update free_spots
-                    self._free_spots.remove(current_pos)
-                    self._free_spots.add(origin_pos)
+                    self._free_spots.remove(pos)
+                    self._free_spots.add(case * self._width + column)
                 # merge with the bottom tile if the values are identical
-                if new_case < self._height - 1 and self._grid[new_case][column] == self._grid[new_case+1][column]:
-                    # WHILE INSTEAD OF IF ? (in the real game its just an if but we can try our own rice here)
-                    self._grid[new_case][column] = 0
-                    self._grid[new_case + 1][column] += 1
-                    self._free_spots.add(current_pos)
+                if y_pos < self._height - 1 and self._grid[y_pos][column] == self._grid[y_pos + 1][column]:
+                    self._grid[y_pos][column] = 0
+                    self._grid[y_pos + 1][column] += 1
+                    self._free_spots.add(pos)
 
     def tmp_up(self) -> None:
         """ TEMPORARY
@@ -182,25 +206,26 @@ class Game:
         """
         for column in range(self._width):
             for case in range(1, self._height):
-                current_pos = origin_pos = case * self._width + column
-                if current_pos in self._free_spots:
+                y_pos = case
+                # stop if case is empty
+                pos = column + case * self._width
+                if pos in self._free_spots:
                     continue
-                current_pos -= self._width
-                while current_pos in self._free_spots:
-                    current_pos -= self._width
-                current_pos += self._width
-                new_case = current_pos // self._width
-                if origin_pos != current_pos:
-                    self._grid[case][column], self._grid[new_case][column] = self._grid[new_case][column], self._grid[case][column]
+                # the tile falls
+                while y_pos > 0 and pos - self._width in self._free_spots:
+                    y_pos -= 1
+                    pos -= self._width
+                if case != y_pos:
+                    # update position in the grid
+                    self._grid[case][column], self._grid[y_pos][column] = self._grid[y_pos][column], self._grid[case][column]
                     # update free_spots
-                    self._free_spots.remove(current_pos)
-                    self._free_spots.add(origin_pos)
+                    self._free_spots.remove(pos)
+                    self._free_spots.add(case * self._width + column)
                 # merge with the bottom tile if the values are identical
-                if new_case > 0 and self._grid[new_case][column] == self._grid[new_case - 1][column]:
-                    # WHILE INSTEAD OF IF ? (in the real game its just an if but we can try our own rice here)
-                    self._grid[new_case][column] = 0
-                    self._grid[new_case - 1][column] += 1
-                    self._free_spots.add(current_pos)
+                if y_pos > 0 and self._grid[y_pos][column] == self._grid[y_pos - 1][column]:
+                    self._grid[y_pos][column] = 0
+                    self._grid[y_pos - 1][column] += 1
+                    self._free_spots.add(pos)
 
     def tmp_left(self) -> None:
         """ TEMPORARY
@@ -287,14 +312,18 @@ class Game:
         """
         return self._grid
 
+
 class GameSettings:
     """represent settings of a game of 2048"""
 
     AVAILABLE_LANGUAGES = {"English"}
+    AVAILABLE_LAYOUTS = {"cross", "square", "linear", "custom"}
 
     def __init__(self, up_key: str, down_key: str,
-    left_key: str, right_key: str, language: str = "English"):
+    left_key: str, right_key: str, language: str = "English",
+    keys_layout: str = "square", crosses: str = None):
         self._build_keys(up_key, down_key, left_key, right_key)
+        self._build_layout(keys_layout, crosses)
         self._build_language(language)
 
     def _build_keys(self, up_key: str, down_key: str,
@@ -315,6 +344,47 @@ class GameSettings:
         and isinstance(right_key, str) and len(right_key) == 1)
         self._keys = [up_key, down_key, left_key, right_key]
 
+    def _build_layout(self, layout: str = "square", crosses: str = None) -> None:
+        """
+        build the layout of the keys to display each turn of the game
+        AVAILABLES:
+            {"cross", "square", "linear", "custom"}
+        CUSTOM:
+            if layout == custom: the arg is a string that contains letters U, D, L and R
+            (up down left right) and as much blank caracters as you want. Ex:
+                LUR
+                 D
+        ARGS:
+            - layout: the layout type (see AVAILABLE above)
+            - crosses: ONLY if layout == custom, is the manual layout of the keys
+        """
+        match layout:
+            case "cross": # ex: wasd on Qwerty keyboards
+                self._layout = f"   [{self.up_key.upper()}]   \n[{self.left_key.upper()}][{self.down_key.upper()}][{self.right_key.upper()}]"
+            case "square": # default
+                self._layout = f"[{self.up_key.upper()}][{self.down_key.upper()}]\n[{self.left_key.upper()}][{self.right_key.upper()}]"
+            case "linear": # ex: VIM keys
+                self._layout = f"[{self.left_key.upper()}][{self.down_key.upper()}][{self.up_key.upper()}][{self.right_key.upper()}]"
+            case "custom":
+                if crosses:
+                    self._build_custom(crosses)
+                else:
+                    raise GameError("building a custom layout require a manual layout"
+                    " as last argument of the constructor\nEXAMPLE:\nLUR\n D ")
+            case _:
+                raise GameError(f"unkown layout: {layout[:16] + ('...' if len(layout) > 16 else '')}")
+
+    def _build_custom(crosses: str) -> None:
+        """
+        build the layout as custom
+        crosses ARG:
+            the crosses argument is a string that contains exactly 4 "X"
+            and as much blank caracters as you want. Ex:
+                X  X
+                 XX
+        """
+        self._layout = ""
+
     def _build_language(self, language: str):
         """
         build the language of the settings
@@ -332,6 +402,14 @@ class GameSettings:
         "\n".join([f"    {Game.AVAILABLE_DIRECTIONS[i].upper()} : {self._keys[i]}" for i in range(4)]) +
         f"\n  Language:\n    {self._language}"
         )
+
+    def __eq__(self, gs2: "GameSettings") -> bool:
+        """
+        return self == gs2
+        ARG:
+            - gs2: the other game settings
+        """
+        return self._keys == gs2._keys and self._language == gs2._language
 
     @property
     def keys(self) -> list[str]:
@@ -355,11 +433,25 @@ class GameSettings:
         return self._keys[1]
 
     @property
+    def left_key(self) -> str:
+        """
+        return the RIGHT key
+        """
+        return self._keys[2]
+
+    @property
     def right_key(self) -> str:
         """
-        return the DOWN key
+        return the LEFT key
         """
-        return self._keys[1]
+        return self._keys[3]
+
+    @property
+    def layout(self) -> str:
+        """
+        return the layout of the directonal keys
+        """
+        return self._layout
 
     @property
     def language(self) -> str:
@@ -378,98 +470,45 @@ def run_game(settings: GameSettings):
     g = Game()
     g.spawn_random(2, "start")
     while True:
-        print("\n")
         g.display()
-        print("use the Z Q S D keys\n   [Z]   \n[Q][S][D]")
+        print(f"use the Z Q S D keys\n{settings.layout}")
         direction = input("choose a direction: ")
         match direction.lower():
-            case "z":
+            case settings.up_key:
                 g.change_gravity(0)
-            case "s":
+            case settings.down_key:
                 g.change_gravity(1)
-            case "d":
-                g.change_gravity(3)
-            case "q":
+            case settings.left_key:
                 g.change_gravity(2)
+            case settings.right_key:
+                g.change_gravity(3)
             case _:
-                print(f"unkown direction: [{direction.upper()[0]}{"..." if len(direction) > 1 else ""}]")
+                print(f"unkown direction: [{(direction.upper()[0] + "..." if len(direction) > 1 else "") if len(direction) else ""}]")
         if g.is_full():
             break
         g.spawn_random(2)
-
-    print("\nyou loose")
-
-def qwerty():
-    g = Game()
-    g.spawn_random(2, "start")
-    while True:
-        g.display()
-        print("use the WASD keys\n   [W]   \n[A][S][D]")
-        direction = input("choose a direction: ")
-        match direction.lower():
-            case "w":
-                g.change_gravity(0)
-            case "s":
-                g.change_gravity(1)
-            case "d":
-                g.change_gravity(3)
-            case "a":
-                g.change_gravity(2)
-            case _:
-                print(f"unkown direction: [{direction.upper()[0]}{"..." if len(direction) > 1 else ""}]")
-        if g.is_full():
-            break
-        g.spawn_random(2)
-        print("\n")
-
-    print("\nyou loose")
-
-def vim():
-    g = Game()
-    g.spawn_random(2, "start")
-    while True:
-        print("\n")
-        g.display()
-        print("use the VIM keys\n[H][J][K][L]")
-        direction = input("choose a direction: ")
-        match direction.lower():
-            case "k":
-                g.change_gravity(0)
-            case "j":
-                g.change_gravity(1)
-            case "l":
-                g.change_gravity(3)
-            case "h":
-                g.change_gravity(2)
-            case _:
-                print(f"unkown direction: [{direction.upper()[0]}{"..." if len(direction) > 1 else ""}]")
-        if g.is_full():
-            break
-        g.spawn_random(2)
+        print()
 
     print("\nyou loose")
 
 def main():
     """run the program"""
-    if len(sys.argv) == 1:
-        azerty() # I am belgian
+    if "--help" in sys.argv[1:]:
+        print(help_msg)
+    elif "--azerty" in sys.argv[1:]:
+        run_game(GameSettings("z", "s", "q", "d", keys_layout="cross"))
+    elif "--qwerty" in sys.argv[1:]:
+        run_game(GameSettings("w", "s", "a", "d", keys_layout="cross"))
+    elif "--vim" in sys.argv[1:]:
+        run_game(GameSettings("k", "j", "h", "l", keys_layout="linear"))
     else:
-        if "--help" in sys.argv[1:]:
-            print(help_msg)
-        elif "--azerty" in sys.argv[1:]:
-            azerty()
-        elif "--qwerty" in sys.argv[1:]:
-            qwerty()
-        elif "--vim" in sys.argv[1:]:
-            vim()
-        else:
-            azerty()
+        run_game(GameSettings("z", "s", "q", "d", keys_layout="cross"))
 
 
 if __name__ == "__main__":
-    azerty_settings = GameSettings("z", "s", "q", "d")
-    print(azerty_settings)
-    #main()
+    #azerty_settings = GameSettings("z", "s", "q", "d")
+    #print(azerty_settings)
+    main()
 
 #{TODO}
 # 1. fix up and down movements (the while loop use the pos and its just worng because it can jump columns)
